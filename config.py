703ef50f0b9e052428cadeb4f2e4bda3e67ab9a0
Ershar Teferdet <103109561+Teferdet@users.noreply.github.com>
2022-11-29 20:37:55 +0200

    finances 3.1 patch 1

---
+++ config.py
@@ -0,0 +1,23 @@
+# Token, API key
+token = "<>"
+api_crypto_key = "<>"
+
+# ID
+log_id = "<>"
+ID = "<>"
+
+# Url
+github = '<>'
+telegram = "<>"
+news = "<>"
+
+#block list 
+block_currency_list = ["RUB", "BYR", "HUF", "RSD"]
+
+#file id
+glory_to_Ukraine = "CQACAgIAAxkDAAIubGOCJZgF4mMbPRN-QKVT2tiitxIxAALiIgACbLcRSBXRRVXWS1TuKwQ"
+anthem_of_Ukraine = "CQACAgIAAxkDAAIuY2OCJD9idz3aOpqFecyOi0HZq3FjAALfIgACbLcRSO2PlW0QPv-0KwQ"
+
+# name
+version = "3.1 patch 1"
+database = "UserInfo.db"
