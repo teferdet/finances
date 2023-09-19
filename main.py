69b0e4f1ba33e989c55c8afb54117ce81505388b
Ershar Teferdet <103109561+Teferdet@users.noreply.github.com>
2023-09-19 09:31:24 +0300

    finances 3.7.1

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ import parser
 import message_handler
 import inline_mode
 
+params = {
+    "timeout": 10,
+    "long_polling_timeout": 5
+}
+
 if __name__ == '__main__':
-    threading.Thread(target=bot.infinity_polling).start()
+    threading.Thread(target=bot.infinity_polling, kwargs=params).start()
     threading.Thread(target=parser.main).start()
